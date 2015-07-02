1f4b75bc24f66ffd79187ca43941ba002e106439
abovethecloud <claudio.peroni93@gmail.com>
2015-07-02 12:04:40 +0200

    Server Web:
    Starting to add resources to web folder

--- ServerWeb/HttpServer/src/tests/ServerLauncher.java
+++ ServerWeb/HttpServer/src/tests/ServerLauncher.java
@@ -7,23 +7,23 @@ import server.dataHandler.TextFileHandler;
 
 public class ServerLauncher {
 
-	public static final String INDEX = "web/index.html";
+	public static final String HOME = "web/Home.html";
 	public static final String ERROR = "web/error404.html";
 	public static final int PORT = 4444;
 
 	public static void main(String[] args) {
 
 		Server server = new Server(PORT);
-		server.addService("html", new FileService(new TextFileHandler(), INDEX,
+		server.addService("html", new FileService(new TextFileHandler(), HOME,
 				ERROR));
-		server.addService("js", new FileService(new TextFileHandler(), INDEX,
+		server.addService("js", new FileService(new TextFileHandler(), HOME,
 				ERROR));
-		server.addService("css", new FileService(new TextFileHandler(), INDEX,
+		server.addService("css", new FileService(new TextFileHandler(), HOME,
 				ERROR));
 		server.addService("png", new FileService(new ImageFileHandler("png"),
-				INDEX, ERROR));
+				HOME, ERROR));
 		server.addService("jpg", new FileService(new ImageFileHandler("jpg"),
-				INDEX, ERROR));
+				HOME, ERROR));
 		server.launch();
 
 	}
